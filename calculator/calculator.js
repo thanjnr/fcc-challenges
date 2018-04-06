0e12c8b0e654b3d15f4313b668ca588fa7bbd8f8
Thomas Han <ethomas.han@gmail.com>
2018-04-06 22:25:36 +1200

    Clear implementation

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,30 +1,30 @@
 'use strict'
 
-var calculator = (function() {
+var calculator = (function () {
     var previousInput = '';
     var operators = [42, 43, 45, 47, 61];
-    var allowedKeys = [13, 42, 43, 45, 46, 47, 61 , 48, 49, 50, 51, 52, 53, 54, 55, 56, 57];
+    var allowedKeys = [13, 42, 43, 45, 46, 47, 61, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57];
     //47:divide 42:multiply 45:subtract 43:add
     const OPERATION_REGEX = /[+\-\/*]/;
 
-    function add(num1, num2) {
-        return num1 + num2;
-    }
-    
-    function subtract(num1, num2) {
-        return num1 - num2;
-    }
-    
-    function multiply(num1, num2) {
-        return num1 * num2;
-    }
-    
-    function divide(num1, num2) {
-        return num1 / num2;
-    }
-    
     function calculation(num, operation) {
-        return function(second_num) {
+        function add(num1, num2) {
+            return num1 + num2;
+        }
+
+        function subtract(num1, num2) {
+            return num1 - num2;
+        }
+
+        function multiply(num1, num2) {
+            return num1 * num2;
+        }
+
+        function divide(num1, num2) {
+            return num1 / num2;
+        }
+
+        return function (second_num) {
             num = Number(num);
             second_num = Number(second_num);
 
@@ -33,8 +33,8 @@ var calculator = (function() {
             }
             if (operation === '-') {
                 return subtract(num, second_num);
-            } 
-            if(operation === '*') {
+            }
+            if (operation === '*') {
                 return multiply(num, second_num);
             }
             if (operation === '/') {
@@ -45,28 +45,30 @@ var calculator = (function() {
     }
 
     function attachInputEventListener() {
-        window.addEventListener('keypress', keyPressed);        
         const keys = Array.from(document.querySelectorAll('.key'));
+
         keys.forEach(key => key.addEventListener('transitionend', removeTransition));
         keys.forEach(key => key.addEventListener('click', mousePress));
+        window.addEventListener('keypress', keyPressed);
     }
 
-    function keyPressed(event) {        
+    function keyPressed(event) {
         playSound();
         updateCalculation(event.keyCode);
-    }    
+    }
 
     function mousePress(e) {
-        var input = Number.parseFloat(e.target.attributes['data-value'].value);
         playSound();
+        var dataValue = e.target.attributes['data-value'].value
+        var input = dataValue != "ac" && dataValue != "ce" ? Number.parseFloat(dataValue) : dataValue;
         updateCalculation(input);
     }
-    
+
     function removeTransition(e) {
         if (e.propertyName !== 'transform') return;
         e.target.classList.remove('playing');
     }
-    
+
     function playSound() {
         const audio = document.querySelector(`audio[data-key="76"]`);
         if (!audio) return;
@@ -75,33 +77,49 @@ var calculator = (function() {
     }
 
     function updateCalculation(keyCode) {
+        if (keyCode === "ac" || keyCode === "ce") {
+            console.log("RESET");
+            previousInput = "";            
+            updateResult("0");         
+            updateHistory("", true);
+            return;
+        }
+
         if (allowedKeys.indexOf(keyCode) < 0) {
             return;
         }
 
         var currentInput = String.fromCharCode(keyCode);
         updateHistory(currentInput);
-        previousInput = previousInput + currentInput;        
+        previousInput = previousInput + currentInput;
         console.log(previousInput);
-        
-        if(!isAccetableNumber(currentInput)) {
+
+        if (!isAccetableNumber(currentInput)) {
             var operrands = previousInput.split(OPERATION_REGEX);
             var matchedOperation = previousInput.match(OPERATION_REGEX);
-            
-            if(operrands.length > 1) {
-                var calc = calculation(operrands[0], matchedOperation[0]);
-                var result = calc(operrands[1]);                
-                var resultElement = document.querySelector('.result h1');
 
-                resultElement.innerHTML = result;                
+            if (operrands.length > 1) {
+                var calc = calculation(operrands[0], matchedOperation[0]);
+                var result = calc(operrands[1]);
+                updateResult(result);
                 previousInput = result + currentInput;
             }
-        } 
+        }
     }
 
-    function updateHistory(currentInput) {
+    function updateHistory(currentInput, reset = false) {
         var historyElement = document.getElementById('history');
+
+        if (reset) {
+            historyElement.innerHTML = "";
+            return;
+        }
         historyElement.innerHTML = historyElement.innerHTML + currentInput;
+    }
+
+    function updateResult(result) {        
+        var resultElement = document.querySelector('.result h1');
+        resultElement.innerHTML = result;
     }
 
     function isAccetableNumber(value) {
